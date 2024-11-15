17794bf2b4c7cea4ae9cf94edb1aaec6c246187d
Fufu <fufuok@qq.com>
2024-11-15 11:07:55 +0800

    chore: improve main flag parsing

--- master/main.go
+++ master/main.go
@@ -11,22 +11,28 @@ import (
 	"github.com/fufuok/pkg/config"
 )
 
-var version, daemon bool
+var (
+	// FlagParser 可由 App 指定自定义的 flag
+	FlagParser = flagParse
+
+	Daemon  bool
+	Version bool
+)
 
 func flagParse() {
 	flag.StringVar(&config.RootPath, "p", config.DefaultRootPath, "程序启动目录(可选)")
 	flag.StringVar(&config.ConfigFile, "c", config.ConfigFile, "主配置文件绝对路径(可选)")
-	flag.BoolVar(&config.Debug, "debug", false, "全局调试模式, 控制台 DEBUG 日志")
-	flag.BoolVar(&daemon, "d", false, "启动后台守护进程")
-	flag.BoolVar(&version, "v", false, "版本信息")
+	flag.BoolVar(&config.Debug, "debug", config.Debug, "开发者调试模式, 控制台 Debug 日志")
+	flag.BoolVar(&Daemon, "d", Daemon, "启动后台守护进程")
+	flag.BoolVar(&Version, "v", Version, "版本信息")
 	flag.Parse()
 }
 
 // Main 带默认命令行参数启动
 func Main() {
-	flagParse()
+	FlagParser()
 
-	if version {
+	if Version {
 		fmt.Println(">>>", config.AppName, config.Version, config.GoVersion)
 		fmt.Println(">>>", config.GitCommit)
 		return
@@ -37,7 +43,7 @@ func Main() {
 
 // Run 守护进程启动程序
 func Run() {
-	if daemon && !config.Debug {
+	if Daemon && !config.Debug {
 		xdaemon.NewDaemon(config.LogDaemon).Run()
 	}
 
